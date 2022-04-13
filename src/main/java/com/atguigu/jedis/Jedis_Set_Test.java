0243499d8706f8ced1405cf4cf74f303a3fe0da5
AdamMa <Adam_Ma520@outlook.com>
2022-04-13 11:31:05 +0800

    1 commit 3

---
+++ src/main/java/com/atguigu/jedis/Jedis_Set_Test.java
@@ -0,0 +1,19 @@
+package com.atguigu.jedis;
+
+/**
+ * @author Adam-Ma
+ * @date 2022/4/13 11:29
+ * @Project jedis_demo
+ * @email Adam_Ma520@outlook.com
+ * @phone 18852895353
+ */
+
+/**
+ *   redis  中 的 set 数据结构
+ *      sadd
+ *      smembers
+ *      sismember
+ *
+ */
+public class Jedis_Set_Test {
+}
