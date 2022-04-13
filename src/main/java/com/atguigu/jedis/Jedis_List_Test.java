004b5a6a1342301ea3a585692dad39f3a0590641
AdamMa <Adam_Ma520@outlook.com>
2022-04-13 10:39:28 +0800

    1 commit

---
+++ src/main/java/com/atguigu/jedis/Jedis_List_Test.java
@@ -0,0 +1,19 @@
+package com.atguigu.jedis;
+
+/**
+ * @author Adam-Ma
+ * @date 2022/4/13 10:38
+ * @Project jedis_demo
+ * @email Adam_Ma520@outlook.com
+ * @phone 18852895353
+ */
+
+/**
+ *  测试 Jedis 中的 List 数据类型
+ *      lpush/rpush
+ *      lpop / rpop
+ *
+ *
+ */
+public class Jedis_List_Test {
+}
